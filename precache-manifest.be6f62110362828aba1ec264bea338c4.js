12cd90f66bdb4ca320b72552389d26c68b4918fc
Preeti <preetitanu5@gmail.com>
2022-07-10 22:04:11 +0530

    Updates

---
+++ precache-manifest.be6f62110362828aba1ec264bea338c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b3ff71b9b181b18adc0",
+    "url": "/Task-Manager/static/css/main.2420bec1.chunk.css"
+  },
+  {
+    "revision": "0b3ff71b9b181b18adc0",
+    "url": "/Task-Manager/static/js/main.0b3ff71b.chunk.js"
+  },
+  {
+    "revision": "43a331ba0ebecdc06cc3",
+    "url": "/Task-Manager/static/js/1.43a331ba.chunk.js"
+  },
+  {
+    "revision": "074c04114a0ef74e42e5",
+    "url": "/Task-Manager/static/js/runtime~main.074c0411.js"
+  },
+  {
+    "revision": "4d6ba97bc05fe04055dd5938ff65444f",
+    "url": "/Task-Manager/index.html"
+  }
+];
